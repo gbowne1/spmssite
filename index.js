212d5e0ede30252a8da31004d1173fc82339390c
Rmichal503 <rakmichal503@gmail.com>
2022-08-23 22:04:35 +0200

    navbar v2

--- index.js
+++
@@ -1,9 +0,0 @@
-var hamburger = document.getElementById("hamburger");
-var hamburgerTrigger = document.getElementById("hamburger_trigger");
-hamburgerTrigger.addEventListener("click", () => {
-  if (hamburger.style.display == "none") {
-    hamburger.style.display = "flex";
-  } else {
-    hamburger.style.display = "none";
-  }
-});
